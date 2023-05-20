f25004e1705553762940c942d6303891eaa760cf
Nenad Cetic <chedadsp@gmail.com>
2023-05-20 14:13:48 +0200

    cv2 as cv

--- control/camera.py
+++ control/camera.py
@@ -1,15 +1,15 @@
-import cv2
+import cv2 as cv
   
   
 # define a video capture object
-vid = cv2.VideoCapture(0)
+vid = cv.VideoCapture(0)
 
 ret, frame = vid.read()
 
 # Display the resulting frame
-cv2.imwrite("test.jpg", frame)
+cv.imwrite("test.jpg", frame)
 
 # After the loop release the cap object
 vid.release()
 # Destroy all the windows
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
